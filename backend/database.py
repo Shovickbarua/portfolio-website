fbab2702faab58d829f6dbabdb5e01bf6a2a63a1
Shovickbarua <shovickbarua92@gmail.com>
2024-09-06 17:33:22 +0600

    update

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = 'mysql+pymysql://root:123456@localhost:3306/message'
+URL_DATABASE  = 'mysql+pymysql://root:123456@localhost:3306/message'
 
 engine = create_engine(URL_DATABASE)
 
