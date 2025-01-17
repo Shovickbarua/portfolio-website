c461259afa306fc35948c954d784c5c8fb345b80
Shovickbarua <shovickbarua92@gmail.com>
2025-01-17 20:23:03 +0600

    update

---
+++ api/main.py
@@ -0,0 +1,65 @@
+from fastapi import Depends, FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+origins = [
+    "https://shovickbarua.info",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Contact(BaseModel):
+    name : str
+    email : str
+    message : str
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+@app.post("/contact")
+async def contact(contact: Contact):
+    return {"message": "Hello World"}
+
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
